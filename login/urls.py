407bec1eb80a6f95fb670af443a0dc1db1a6eeb7
Marcus <omarcusvinicius96@gmail.com>
2023-10-09 15:07:16 -0500

    add url, view e model do cad cliente, view incompl

--- login/urls.py
+++ login/urls.py
@@ -6,5 +6,6 @@ from login import views
 app_name = 'login'
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login_cliente/', views.login_cliente, name='login_cliente')
+    path('login_cliente/', views.login_cliente, name='login_cliente'),
+    path('cadastro_cliente/', views.cadastro_cliente, name='cadastro_cliente')
 ]
--- login/views.py
+++ login/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from .models import Cliente
 from django.contrib import messages
+from django.contrib.auth.models import User
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
@@ -18,3 +19,26 @@ def login_cliente(request):
         else:
             messages.error(request, 'Erro no telefone ou senha')
     return render(request, 'login_cliente.html')
+
+def cadastro_cliente(request):
+    if request.POST:
+        username = request.POST.get('login')
+        senha = request.POST.get('senha')
+        nome = request.POST.get('nome')
+        telefone = request.POST.get('telefone')
+        user = User.objects.filter(username=username).first() # Verifica se o usuário já existe         
+        # Cria o usuário e registra com adm
+        if user is None:
+            user = User.objects.create_user(username=username,  telefone=telefone)
+            user.set_password(senha)
+            user.is_active = True
+            user.save()
+            user
+            cliente = Cliente(usuario=user, nome=nome)
+            cliente.save()
+            return redirect("login:login_adm")
+        else:
+            messages.error(request, "Usuário já cadastrado")
+            return render(request, 'cadastro_cliente.html')
+    
+    return render(request, 'cadastro_cliente.html')
