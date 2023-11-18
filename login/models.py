498dbe0876ab0ddb81bdfea136ca794109b33e15
sivirino <marcus.oliveira@escolar.ifrn.edu.br>
2023-11-17 21:57:29 -0500

    model de vendedor e produto

--- login/models.py
+++ login/models.py
@@ -3,9 +3,22 @@ from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
 
 class Cliente(models.Model):
-    nome = models.CharField(max_length=15)
-    telefone = models.CharField(max_length=11, unique=True)
+    nome = models.CharField(max_length=50)
+    telefone = models.CharField(max_length=15, unique=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.nome
+    
+class Vendedor(models.model):
+    nome = models.CharField(max_length=50)
+    telefone = models.CharField(max_length=15, unique=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    disponibilidade = models.BooleanField(default=True)
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=50)
+    sabor = models.CharField(max_length=50)
+    quantidade = models.IntegerField(default=0)
+    preco = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    
