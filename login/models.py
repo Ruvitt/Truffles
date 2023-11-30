06b658e29dcfc57c3e57e8db18f22f4223e802b9
sivirino <marcus.oliveira@escolar.ifrn.edu.br>
2023-11-30 14:45:03 -0500

    concertando model, view e templ de cad de vendedor

--- login/models.py
+++ login/models.py
@@ -14,7 +14,6 @@ class Vendedor(models.Model):
     nome = models.CharField(max_length=50)
     telefone = models.CharField(max_length=15, unique=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    disponibilidade = models.BooleanField(default=True)
 
 def __str__(self):
         return self.nome
