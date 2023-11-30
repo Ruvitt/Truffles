caab0ef7f99f4ed1484c56558e56eca255b6a65e
sivirino <marcus.oliveira@escolar.ifrn.edu.br>
2023-11-30 14:45:43 -0500

    adicionando class sabor na class produto

--- menu/models.py
+++ menu/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Sabor (models.TextChoices):
+    chocolate = 'chocolate', 'Chocolate'
+    doce_de_leite = 'doce_de_leite', 'Doce de leite'
+    morango = 'morango', 'Morango'
+
 # Create your models here.
 class Produto (models.Model):
     nome = models.CharField(max_length=50)
@@ -8,13 +13,7 @@ class Produto (models.Model):
     preco = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
     quantidade = models.IntegerField()
     imagem = models.ImageField(upload_to='media/')
-    SABOR_CHOICES = [
-        ('Morango', 'Chocolate'),
-        ('Limão', 'Ninho'),
-        ('Doce de leite', 'Goiaba'),
-        # Adicione mais opções conforme necessário
-    ]
-    sabor = models.CharField(max_length=20, choices=SABOR_CHOICES)
+    sabor = models.CharField(max_length=20, choices=Sabor.choices)
     
     def __str__(self):
         return self.nome
